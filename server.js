bafc200e5df3a95caecd7139ccfdb6beca94c283
Andrew Robida <afrobida@gmail.com>
2018-01-12 23:34:54 +0000

    idk

--- server.js
+++ server.js
@@ -4,28 +4,32 @@ const compression = require('compression');
 const morgan = require('morgan');
 const path = require('path');
 
+
 const normalizePort = port => parseInt(port, 10);
 const PORT = normalizePort(process.env.PORT || 5000);
 
 const app = express();
 const dev = app.get('env') !== 'production';
 
-const { Client } = require('pg');
+
+//For initializing pg db on heroku
+/*const { Client } = require('pg');
 
 const client = new Client({
     connectionString: process.env.DATABASE_URL,
     ssl: true,
-});
+  });
 
-client.connect();
+  client.connect();
 
-client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+  client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
     if (err) throw err;
     for (let row of res.rows) {
-        console.log(JSON.stringify(row));
+      console.log(JSON.stringify(row));
     }
     client.end();
-});
+  });*/
+
 
 if (!dev) {
     app.disable('x-powered-by');
